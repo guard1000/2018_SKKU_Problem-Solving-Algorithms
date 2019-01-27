f97004f8d894cdf7fddcfe7985c5aaa1365fb43a
guard1000 <cjsdnr885@naver.com>
2019-01-27 18:14:19 +0900

    Create 6주차_과제.py

---
+++ 6주차_과제.py
@@ -0,0 +1,16 @@
+#num = int(input('1과0으로만 구성된 수를 입력하세요: '))
+#count = 0
+
+#while(num>0):
+#    if num%10 == 1:
+#        count= count+1
+#    num = num//10
+
+#print('1이 %d개 포함되어 있습니다.' %count)
+
+
+num = 1010
+print ('num: %d' %num)
+
+print('num을 10으로 나눈 묷 = %d' %(num//10))
+print('num을 10으로 나눈 나머지 = %d' %(num%10))
