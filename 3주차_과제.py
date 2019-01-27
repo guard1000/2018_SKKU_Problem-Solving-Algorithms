3a2332d356758a1ed2b9de935f2b261ebf5b4e79
guard1000 <cjsdnr885@naver.com>
2019-01-27 18:14:05 +0900

    Create 3주차_과제.py

---
+++ 3주차_과제.py
@@ -0,0 +1,8 @@
+jumsu = {}
+jumsu['국어']=int(input('국어: '))
+jumsu['수학']=int(input('수학: '))
+jumsu['영어']=int(input('영어: '))
+jumsu['평균']=(jumsu['국어']+jumsu['수학']+jumsu['영어'])/3
+
+print(jumsu['국어'],jumsu['수학'],jumsu['영어'],jumsu['평균'])
+
