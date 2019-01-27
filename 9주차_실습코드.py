a9e2910a4f13cd6e273059127dbdae32e4421305
guard1000 <cjsdnr885@naver.com>
2019-01-27 18:14:44 +0900

    Create 9주차_실습코드.py

---
+++ 9주차_실습코드.py
@@ -0,0 +1,15 @@
+import turtle as t
+t.color('yellow')
+t.begin_fill()
+t.circle(50)
+t.end_fill()
+
+t.up()
+t.forward(200)
+t.down()
+
+import turtle as t
+t. color('pink')
+t.begin_fill()
+t.circle(100)
+t.end_fill()
